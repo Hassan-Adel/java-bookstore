5a8d2d61ece334708f56b9e35f184055617cdd96
hassan.adel <hasanadel555@hotmail.com>
2020-05-13 06:33:52 +0200

    add @Transactional(SUPPORTS) to the class itself

---
+++ src/main/java/repository/BookRepository.java
@@ -0,0 +1,43 @@
+package repository;
+
+import model.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+import static javax.transaction.Transactional.TxType.REQUIRED;
+
+@Transactional(SUPPORTS)
+public class BookRepository {
+    @PersistenceContext(unitName = "bookStorePU")
+    private EntityManager em;
+
+    public Book find(Long id){
+        return em.find(Book.class, id);
+    }
+
+    @Transactional(REQUIRED)
+    public Book create(Book book){
+        em.persist(book);
+        return book;
+    }
+
+    @Transactional(REQUIRED)
+    public void delete(Long id){
+        em.remove(em.getReference(Book.class, id));
+    }
+
+    public List<Book> findAll(){
+        TypedQuery<Book> query = em.createQuery("SELECT b FROM  Book b ORDER BY b.title DESC", Book.class);
+        return query.getResultList();
+    }
+
+    public Long countAll(){
+        TypedQuery<Long> query = em.createQuery("SELECT COUNT(b) FROM Book b", Long.class);
+        return query.getSingleResult();
+    }
+}
